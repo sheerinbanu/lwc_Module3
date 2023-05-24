caa06c0080c0393230d42aeef8b96c78534597d8
Sheerin Banu <sbanu@allurion.com>
2023-05-24 15:51:31 +0200

    stud

--- test-lwc/jest.config.js
+++ test-lwc/jest.config.js
@@ -2,5 +2,8 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
+    moduleNameMapper: {
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+      },
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
